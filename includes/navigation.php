312efa6c014c5f3d5ab9c47c65af758f3c951788
Tusher-ajoy <tusherajo@gmail.com>
2023-04-03 12:10:43 +0600

    adds post edit link on single post view page

--- includes/navigation.php
+++ includes/navigation.php
@@ -25,12 +25,16 @@
                 <li>
                     <a href="admin">Admin</a>
                 </li>
-                <!-- <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li> -->
+
+                <?php
+                // ***Problem
+                if (!isset($_SESSION['user_role'])) {
+                    if (isset($_GET['p_id'])) {
+                        $the_post_id = $_GET['p_id'];
+                        echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
+                    }
+                }
+                ?>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
