5316b51788416f69eeeb098654e708e26f29c32a
Tusher-ajoy <tusherajo@gmail.com>
2023-04-04 18:07:23 +0600

    adds comment count reset on admin

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -76,6 +76,7 @@ if (isset($_POST['checkBoxArray'])) {
                 <th>View</th>
                 <th>Edit</th>
                 <th>Delete</th>
+                <th>Views</th>
             </tr>
         </thead>
         <tbody>
@@ -122,7 +123,7 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td><a href='../post.php?p_id=$post_id'>VIEW</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>EDIT</a></td>";
                 echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete'); \" href='posts.php?delete={$post_id}'>DELETE</a></td>";
-                echo "<td>{$post_views_count}</td>";
+                echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                 echo "</tr>";
             }
             ?>
@@ -137,6 +138,14 @@ if (isset($_GET['delete'])) {
     $query             = "DELETE FROM posts WHERE post_id = $the_post_id";
     $delete_post_query = mysqli_query($connection, $query);
     confirm_query($delete_post_query);
+    header("Location: posts.php");
+}
+
+if (isset($_GET['reset'])) {
+    $the_post_id      = $_GET['reset'];
+    $query            = "UPDATE posts SET post_views_count = 0 WHERE post_id = " . mysqli_real_escape_string($connection, $_GET['reset']);
+    $reset_post_query = mysqli_query($connection, $query);
+    confirm_query($reset_post_query);
     header("Location: posts.php");
 }
 ?>
