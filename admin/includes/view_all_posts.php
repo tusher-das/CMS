e58343f153295b440e5c5c960c2c77d6b50b5a35
Tusher-ajoy <tusherajo@gmail.com>
2023-04-04 17:48:30 +0600

    adds post view functionality

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -92,6 +92,7 @@ if (isset($_POST['checkBoxArray'])) {
                 $post_tags          = $row['post_tags'];
                 $post_comment_count = $row['post_comment_count'];
                 $post_date          = $row['post_date'];
+                $post_views_count   = $row['post_views_count'];
 
                 echo "<tr>";
                 ?>
@@ -121,6 +122,7 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td><a href='../post.php?p_id=$post_id'>VIEW</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>EDIT</a></td>";
                 echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete'); \" href='posts.php?delete={$post_id}'>DELETE</a></td>";
+                echo "<td>{$post_views_count}</td>";
                 echo "</tr>";
             }
             ?>
--- post.php
+++ post.php
@@ -16,6 +16,12 @@
             if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
 
+                $view_query = "UPDATE posts SET post_views_count = post_views_count+1 WHERE post_id = $the_post_id";
+                $send_query = mysqli_query($connection, $view_query);
+                if (!$send_query) {
+                    die("Query Failed " . mysqli_error($connection));
+                }
+
                 $query               = "SELECT * FROM posts WHERE post_id = $the_post_id";
                 $select_a_post_query = mysqli_query($connection, $query);
 
@@ -60,7 +66,10 @@
 
                     <?php
                 }
+            } else {
+                header("Location: index.php");
             }
+
             ?>
 
 
