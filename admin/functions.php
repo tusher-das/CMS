16756733b7957857a1468428d5b4a5a3e5591dc4
Tusher-ajoy <tusherajo@gmail.com>
2023-04-06 16:04:31 +0600

    Improve Registration System: adds more authentication

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+function redirect($location)
+{
+    return header("Location: " . $location);
+}
+
 function users_online()
 {
     if (isset($_GET['onlineusers'])) {
@@ -101,6 +106,113 @@ function checkStatus($table, $column, $status)
     $result = mysqli_query($connection, $query);
     return mysqli_num_rows($result);
 
+}
+
+function isAdmin($username = '')
+{
+    global $connection;
+
+    $query  = "SELECT user_role FROM users WHERE username = '$username'";
+    $result = mysqli_query($connection, $query);
+    confirm_query($result);
+
+    $row = mysqli_fetch_array($result);
+    if ($row['user_role'] == 'admin') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isExistsUsername($username)
+{
+    global $connection;
+
+    $query  = "SELECT username FROM users WHERE username = '$username'";
+    $result = mysqli_query($connection, $query);
+    confirm_query($result);
+
+    if (mysqli_num_rows($result) > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function isExistsEmail($email)
+{
+    global $connection;
+
+    $query  = "SELECT user_email FROM users WHERE user_email = '$email'";
+    $result = mysqli_query($connection, $query);
+    confirm_query($result);
+
+    if (mysqli_num_rows($result) > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function registerUser($username, $email, $password)
+{
+    global $connection;
+    $username = mysqli_real_escape_string($connection, $username);
+    $email    = mysqli_real_escape_string($connection, $email);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    // password encryption related work
+    $query                 = "SELECT randSalt FROM users";
+    $select_randsalt_query = mysqli_query($connection, $query);
+    confirm_query($select_randsalt_query);
+    $row  = mysqli_fetch_array($select_randsalt_query);
+    $salt = $row['randSalt'];
+
+    $password = crypt($password, $salt); //encrypted password
+
+    $query               = "INSERT INTO users (username, user_email, user_password, user_role) ";
+    $query .= "VALUES('{$username}','{$email}','{$password}','subscriber' )";
+    $register_user_query = mysqli_query($connection, $query);
+
+    confirm_query($register_user_query);
+}
+
+function loginUser($username, $password)
+{
+    global $connection;
+
+    $username = trim($username);
+    $password = trim($password);
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $query             = "SELECT * FROM users WHERE username = '{$username}' ";
+    $select_user_query = mysqli_query($connection, $query);
+
+    confirm_query($select_user_query);
+
+    while ($row = mysqli_fetch_assoc($select_user_query)) {
+        $db_user_id        = $row['user_id'];
+        $db_username       = $row['username'];
+        $db_user_password  = $row['user_password'];
+        $db_user_firstname = $row['user_firstname'];
+        $db_user_lastname  = $row['user_lastname'];
+        $db_user_role      = $row['user_role'];
+    }
+
+    $password = crypt($password, $db_user_password);
+
+    if ($username === $db_username && $password === $db_user_password) {
+
+        $_SESSION['username']  = $db_username;
+        $_SESSION['firstname'] = $db_user_firstname;
+        $_SESSION['lastname']  = $db_user_lastname;
+        $_SESSION['user_role'] = $db_user_role;
+
+        redirect("/cms/admin");
+    } else {
+        redirect("/cms/index.php");
+    }
 }
 
 ?>
--- registration.php
+++ registration.php
@@ -1,44 +1,53 @@
 <?php include("includes/db.php"); ?>
 <?php include("includes/header.php"); ?>
+<?php include("./admin/functions.php"); ?>
 
 <?php
-if (isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $email    = $_POST['email'];
-    $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $username = trim($_POST['username']);
+    $email    = trim($_POST['email']);
+    $password = trim($_POST['password']);
 
-    $username = mysqli_real_escape_string($connection, $username);
-    $email    = mysqli_real_escape_string($connection, $email);
-    $password = mysqli_real_escape_string($connection, $password);
+    $error = [
+        'username' => '',
+        'email'    => '',
+        'password' => ''
+    ];
 
-
-    if (!empty($username) && !empty($email) && !empty($password)) {
-
-        $query                 = "SELECT randSalt FROM users";
-        $select_randsalt_query = mysqli_query($connection, $query);
-        if (!$select_randsalt_query) {
-            die("Query Failed. " . mysqli_error($connection));
-        }
-
-        $row  = mysqli_fetch_array($select_randsalt_query);
-        $salt = $row['randSalt'];
-
-        $password = crypt($password, $salt);
-
-        $query               = "INSERT INTO users (username, user_email, user_password, user_role) ";
-        $query .= "VALUES('{$username}','{$email}','{$password}','subscriber' )";
-        $register_user_query = mysqli_query($connection, $query);
-
-        if (!$register_user_query) {
-            die("Query Failed " . mysqli_error($connection) . ' ' . mysqli_errno($connection));
-        }
-
-        $message = "Your Registration has been submitted";
-    } else {
-        $message = "Fields cannot be empty";
+    // username validation
+    if (strlen($username) < 4) {
+        $error['username'] = 'Username needs to be longer then 3 character!';
+    }
+    if ($username == '') {
+        $error['username'] = 'Username cannot be empty!';
+    }
+    if (isExistsUsername($username)) {
+        $error['username'] = 'Username already exists, pick another one!';
     }
 
+    // email validation
+    if ($email == '') {
+        $error['email'] = 'email cannot be empty!';
+    }
+    if (isExistsEmail($email)) {
+        $error['email'] = 'email already exists, <a href="index.php">Login here</a>';
+    }
 
+    if ($password == '') {
+        $error['password'] = 'Password cannot be empty!';
+    }
+
+    foreach ($error as $key => $value) {
+        if (empty($value)) {
+            unset($error[$key]);
+        }
+    } //foreach
+
+    //user registration
+    if (empty($error)) {
+        registerUser($username, $email, $password);
+        loginUser($username, $password);
+    }
 } else {
     $message = "";
 }
@@ -57,27 +66,38 @@ if (isset($_POST['submit'])) {
                         <h1>Registration</h1>
                         <form action="registration.php" method="post" role="form" id="login-form" autocomplete="off">
                             <h6 class="text-center">
-                                <?php echo $message; ?>
                             </h6>
                             <div class="form-group">
                                 <label for="username" class="sr-only">Username</label>
                                 <input type="text" name="username" id="username" class="form-control"
-                                    placeholder="Enter Desired Username">
+                                    placeholder="Enter Desired Username" autocomplete="on"
+                                    value="<?php echo isset($username) ? $username : '' ?>">
+                                <p>
+                                    <?php echo isset($error['username']) ? $error['username'] : '' ?>
+                                </p>
                             </div>
 
                             <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
                                 <input type="email" name="email" id="email" class="form-control"
-                                    placeholder="somebody@example.com">
+                                    placeholder="somebody@example.com" autocomplete="on"
+                                    value="<?php echo isset($email) ? $email : '' ?>">
+
+                                <p>
+                                    <?php echo isset($error['email']) ? $error['email'] : '' ?>
+                                </p>
                             </div>
 
                             <div class="form-group">
                                 <label for="password" class="sr-only">Password</label>
                                 <input type="password" name="password" id="key" class="form-control"
                                     placeholder="Enter Password">
+                                <p>
+                                    <?php echo isset($error['password']) ? $error['password'] : '' ?>
+                                </p>
                             </div>
 
-                            <input type="submit" value="Register" name="submit" id="btn-login"
+                            <input type="submit" value="REGISTER" name="submit" id="btn-login"
                                 class="btn btn-custom btn-lg btn-block">
                         </form>
                     </div>
