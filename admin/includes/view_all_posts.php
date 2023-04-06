660d565e03a814cbe2fd12d21939f6e56036754a
Tusher-ajoy <tusherajo@gmail.com>
2023-04-06 12:13:51 +0600

    Refactoring: implement inner join on view all post

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -82,8 +82,15 @@ if (isset($_POST['checkBoxArray'])) {
         </thead>
         <tbody>
             <?php
-            $query        = "SELECT * FROM posts ORDER BY post_id DESC";
+            // $query = "SELECT * FROM posts ORDER BY post_id DESC";
+            
+
+            $query = "SELECT posts.post_id, posts.post_author, posts.post_title, posts.post_category_id, posts.post_status, posts.post_image, posts.post_tags, posts.post_comment_count, posts.post_date, posts.post_views_count, ";
+            $query .= "categories.cat_id, categories.cat_title ";
+            $query .= "FROM posts LEFT JOIN categories ON posts.post_category_id = categories.cat_id ORDER BY posts.post_id DESC";
+
             $select_posts = mysqli_query($connection, $query);
+
             while ($row = mysqli_fetch_assoc($select_posts)) {
                 $post_id            = $row['post_id'];
                 $post_author        = $row['post_author'];
@@ -96,6 +103,9 @@ if (isset($_POST['checkBoxArray'])) {
                 $post_date          = $row['post_date'];
                 $post_views_count   = $row['post_views_count'];
 
+                $cat_title = $row['cat_title'];
+                $cat_id    = $row['cat_id'];
+
                 echo "<tr>";
                 ?>
                 <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]'
@@ -107,13 +117,15 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td>{$post_title}</td>";
 
 
-                $query         = "SELECT * FROM categories WHERE cat_id = $post_category_id";
-                $post_category = mysqli_query($connection, $query);
-                while ($row = mysqli_fetch_assoc($post_category)) {
-                    $cat_id    = $row['cat_id'];
-                    $cat_title = $row['cat_title'];
-                    echo "<td>{$cat_title}</td>";
-                }
+                // $query         = "SELECT * FROM categories WHERE cat_id = $post_category_id";
+                // $post_category = mysqli_query($connection, $query);
+                // while ($row = mysqli_fetch_assoc($post_category)) {
+                //     $cat_id    = $row['cat_id'];
+                //     $cat_title = $row['cat_title'];
+                //     echo "<td>{$cat_title}</td>";
+                // }
+            
+                echo "<td>{$cat_title}</td>";
 
 
                 echo "<td>{$post_status}</td>";
