6ec773ee042cca1dfdf705e79b2ce06611b7e30e
Tusher-ajoy <tusherajo@gmail.com>
2023-04-06 17:08:07 +0600

    adds fetch post in category page using prepare statements

--- category.php
+++ category.php
@@ -1,6 +1,8 @@
 <?php include('includes/db.php'); ?>
 <?php include('includes/header.php'); ?>
 
+<?php include('./admin/functions.php'); ?>
+
 <!-- Navigation -->
 <?php include('includes/navigation.php'); ?>
 
@@ -17,63 +19,66 @@
             if (isset($_GET['category'])) {
                 $post_category_id = $_GET['category'];
 
-                if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin') {
-                    $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id";
+                if (isAdmin($_SESSION['username'])) {
+                    $stm1 = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ?");
                 } else {
-                    $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id AND post_status = 'published'";
+                    $stm2      = mysqli_prepare($connection, "SELECT post_id, post_title, post_author, post_date, post_image, post_content FROM posts WHERE post_category_id = ? AND post_status = ?");
+                    $published = 'published';
                 }
 
-                $select_a_category_all_post_query = mysqli_query($connection, $query);
+                if (isset($stm1)) {
+                    mysqli_stmt_bind_param($stm1, 'i', $post_category_id);
+                    mysqli_stmt_execute($stm1);
+                    mysqli_stmt_bind_result($stm1, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
+                    $stmt = $stm1;
+                } else {
+                    mysqli_stmt_bind_param($stm2, 'is', $post_category_id, $published);
+                    mysqli_stmt_execute($stm2);
+                    mysqli_stmt_bind_result($stm2, $post_id, $post_title, $post_author, $post_date, $post_image, $post_content);
+                    $stmt = $stm2;
+                }
 
-                if (mysqli_num_rows($select_a_category_all_post_query) < 1) {
+                // $select_a_category_all_post_query = mysqli_query($connection, $query);
+            
+                if (mysqli_stmt_num_rows($stmt) == 0) {
                     echo "<h1 class='text-center'>No posts available</h1>";
-                } else {
-                    while ($row = mysqli_fetch_assoc($select_a_category_all_post_query)) {
-
-                        $post_id      = $row['post_id'];
-                        $post_title   = $row['post_title'];
-                        $post_author  = $row['post_author'];
-                        $post_date    = $row['post_date'];
-                        $post_image   = $row['post_image'];
-                        $post_content = substr($row['post_content'], 0, 100);
-                        ?>
-
-                        <h1 class="page-header">
-                            Page Heading
-                            <small>Secondary Text</small>
-                        </h1>
-
-                        <!-- First Blog Post -->
-                        <h2>
-                            <a href="post.php?p_id=<?php echo $post_id; ?>">
-                                <?php echo $post_title; ?>
-                            </a>
-                        </h2>
-                        <p class="lead">
-                            by <a href="index.php">
-                                <?php echo $post_author; ?>
-                            </a>
-                        </p>
-                        <p><span class="glyphicon glyphicon-time"></span>
-                            <?php echo $post_date; ?>
-                        </p>
-                        <hr>
-                        <a href="post.php?p_id=<?php echo $post_id; ?>">
-                            <img class="img-responsive" width="800" height="200" src="images/<?php echo $post_image; ?>" alt="">
-                        </a>
-                        <hr>
-                        <p>
-                            <?php echo $post_content; ?>
-                        </p>
-                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span
-                                class="glyphicon glyphicon-chevron-right"></span></a>
-
-                        <hr>
-
-                        <?php
-                    }
                 }
+                while (mysqli_stmt_fetch($stmt)):
+                    ?>
 
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="post.php?p_id=<?php echo $post_id; ?>">
+                            <?php echo $post_title; ?>
+                        </a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php">
+                            <?php echo $post_author; ?>
+                        </a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span>
+                        <?php echo $post_date; ?>
+                    </p>
+                    <hr>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>">
+                        <img class="img-responsive" width="800" height="200" src="images/<?php echo $post_image; ?>" alt="">
+                    </a>
+                    <hr>
+                    <p>
+                        <?php echo $post_content; ?>
+                    </p>
+                    <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span
+                            class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+                <?php endwhile;
             } else {
                 header("Locations: index.php");
             }
