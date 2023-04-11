a7171341a1e66a3fc42d2f5ef2d0155bfdc9a462
Tusher-ajoy <tusherajo@gmail.com>
2023-04-11 17:01:11 +0600

    adds mail sending functionality

--- contact.php
+++ contact.php
@@ -2,21 +2,55 @@
 <?php include("includes/header.php"); ?>
 
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require './vendor/autoload.php';
+
+?>
+
+<?php
 
 if (isset($_POST['submit'])) {
-    $email   = $_POST['email'];
+    $name    = $_POST['name'];
     $subject = wordwrap($_POST['subject'], 70);
     $body    = $_POST['body'];
 
-    $subject = mysqli_real_escape_string($connection, $subject);
-    $body    = mysqli_real_escape_string($connection, $body);
-    $to      = "tusherajoy@gmail.com";
+    if (!empty($name) && !empty($subject) && !empty($body)) {
+        $subject = mysqli_real_escape_string($connection, $subject);
+        $body    = mysqli_real_escape_string($connection, $body);
+        $name    = mysqli_real_escape_string($connection, $name);
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'From: ' . $email . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        //cofigure PHPMailer
+        $mail = new PHPMailer(true);
 
-    mail($to, $subject, $body, $headers);
+        $mail->isSMTP();
+        $mail->Host       = Config::SMPT_HOST;
+        $mail->Username   = Config::SMTP_USER;
+        $mail->Password   = Config::SMTP_PASSWORD;
+        $mail->Port       = Config::SMTP_PORT;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth   = true;
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+
+        $mail->setFrom('tusherajoy@gmail.com', $name);
+        $mail->addAddress('tusherajoy@gmail.com');
+
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+
+        if ($mail->send()) {
+            $emailSent = true;
+        } else {
+            $emailSent = false;
+        }
+
+    }
 
 }
 ?>
@@ -29,11 +63,20 @@ if (isset($_POST['submit'])) {
     <div class="row">
         <div class="col-xs-6 col-xs-offset-3">
             <div class="form-wrap">
+                <?php
+                if (isset($emailSent)) {
+                    if ($emailSent === true) {
+                        echo "<div class='alert alert-success'><strong>Well done!</strong> you successfully send the message.</div>";
+                    } else {
+                        echo "<div class='alert alert-danger'><strong>Oh snap!</strong> Change a few things up and try submitting again.</div>";
+                    }
+                }
+                ?>
                 <h1>Contact</h1>
                 <form action="" method="post" role="form" id="login-form" autocomplete="off">
                     <div class="form-group">
-                        <label for="email" class="sr-only">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email">
+                        <label for="name" class="sr-only">Name</label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name">
                     </div>
                     <div class="form-group">
                         <label for="subject" class="sr-only">Subject</label>
