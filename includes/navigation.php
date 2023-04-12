fad5d915e97159b71572fa6ffe89f653ea1da796
Tusher-ajoy <tusherajo@gmail.com>
2023-04-12 15:30:56 +0600

    update navbar style and add background image on body

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,5 +1,6 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-default nav-bg  navbar-fixed-top" role="navigation">
     <div class="container">
+
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse"
@@ -9,11 +10,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/cms">CMS Front</a>
+            <a class="navbar-brand logo" href="/cms">CodeFlow</a>
         </div>
+
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+                <!-- categories -->
                 <?php
                 $query                       = "SELECT * FROM categories";
                 $select_all_categories_query = mysqli_query($connection, $query);
@@ -38,7 +41,7 @@
                     }
                     echo "<li class='$category_class'><a href='/cms/category/$cat_id'>{$cat_title}</a></li>";
                 }
-                ?>
+                ?> <!-- ./categories -->
 
                 <li class='<?php echo $contact_class; ?>'>
                     <a href="/cms/contact">Contact</a>
@@ -60,30 +63,21 @@
                     </li>
 
                 <?php endif; ?>
-
-                <?php
-                // ***Problem
-                if (!isset($_SESSION['user_role'])) {
-                    if (isset($_GET['p_id'])) {
-                        $the_post_id = $_GET['p_id'];
-                        echo "<li><a href='/cms/admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
-                    }
-                }
-                ?>
             </ul>
 
             <!-- Blog Search -->
-            <form class="navbar-form navbar-left" action="search.php" method="post">
+            <form class="navbar-form navbar-right" action="search.php" method="post">
                 <div class="input-group">
                     <input type="text" name="search" class="form-control" placeholder="Search...">
                     <span class="input-group-btn">
                         <button name="submit" class="btn btn-default" type="submit">
-                            <span class="glyphicon glyphicon-search"></span>
+                            <span class="glyphicon glyphicon-search search-icon"></span>
                         </button>
                     </span>
                 </div>
             </form>
         </div>
+
         <!-- /.navbar-collapse -->
     </div>
     <!-- /.container -->
