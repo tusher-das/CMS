ccb936a9b3d2f22c537d7e496437f16e23bf98ac
Tusher-ajoy <tusherajo@gmail.com>
2023-04-13 12:58:52 +0600

    adds background image and style welcome message

--- admin/includes/admin_heading.php
+++ admin/includes/admin_heading.php
@@ -1,7 +1,7 @@
 <div class="col-lg-12">
     <h1 class="page-header">
         Welcome to admin
-        <small>
+        <small class="highlight">
             <?php echo $_SESSION['firstname'] ?>
         </small>
     </h1>
